4ccdd066d90d0b4cc4498877cfc93be05a374be4
erianvc <erianvc.dev@gmail.com>
2022-07-22 21:07:14 -0500

    feat: agrega el componente `Button`

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Title from './components/Title.jsx'
+import Button from './components/Button.jsx'
 
 function App () {
   return (
     <main>
       <Title>QUIZ GAME</Title>
+      <Button>LOG IN</Button>
+      <Button>SIGN UP</Button>
     </main>
   )
 }
